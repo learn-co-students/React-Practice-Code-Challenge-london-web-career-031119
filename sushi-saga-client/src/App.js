59dfb8d3c5a58325ac208e5306d3a2222a444c27
idanceinbetween <46828701+idanceinbetween@users.noreply.github.com>
2019-05-20 17:06:38 +0100

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -30,11 +30,10 @@ class App extends Component {
     if (!this.state.eatenSushis.find(s => s.id === sushi.id)) {
       const credits = this.state.credits - sushi.price;
       if (credits >= 0) {
-        this.setState({ credits });
-
-        const eatenSushis = [...this.state.eatenSushis];
-        eatenSushis.push(sushi);
-        this.setState({ eatenSushis });
+        this.setState({
+          credits,
+          eatenSushis: [...this.state.eatenSushis, sushi]
+        });
 
         const availableSushis = [...this.state.availableSushis];
         const sushiIndex = availableSushis.indexOf(
@@ -42,13 +41,15 @@ class App extends Component {
         );
         availableSushis.splice(sushiIndex, 1); //remove selected sushi from availableSushis
         this.setState({ availableSushis });
+      } else {
+        alert("Please pick a cheaper sushi or topup!");
       }
     }
   };
 
   handleDisplayedSushis = () => {
     let n = this.state.displayedSushis[3].id;
-    if (n >= 100) {
+    if (n >= this.state.allSushis.length) {
       n = 0;
     }
     const allSushis = [...this.state.allSushis];
