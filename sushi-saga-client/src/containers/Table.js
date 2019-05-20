5a54938a2743aecba7566a615757209e05d1f678
idanceinbetween <46828701+idanceinbetween@users.noreply.github.com>
2019-05-20 11:55:28 +0100

    Done.

--- sushi-saga-client/src/containers/Table.js
+++ sushi-saga-client/src/containers/Table.js
@@ -1,32 +1,29 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from "react";
 
-const Table = (props) => {
-
-  const renderPlates = (array) => {
+const Table = props => {
+  const renderPlates = array => {
     return array.map((x, index) => {
-      return <div className="empty-plate" style={{ top: -7 * index }}/>
-    })
-  }
+      return (
+        <div className="empty-plate" style={{ top: -7 * index }} key={index} />
+      );
+    });
+  };
 
   return (
     <Fragment>
-      <h1 className="remaining">
-        You have: ${ /* Give me how much money I have left */ } remaining!
-      </h1>
+      <div className="remaining">
+        <h1>You have: ${props.credits} remaining!</h1>
+        <form className="remaining" onSubmit={e => props.onTopup(e)}>
+          $
+          <input type="integer" placeholder="Enter amount here" name="amount" />
+          <input type="submit" value="Top Up Your Sushi Wallet" />
+        </form>
+      </div>
       <div className="table">
-        <div className="stack">
-          {
-            /* 
-               renderPlates takes an array 
-               and renders an empty plate
-               for every element in the array
-            */
-            renderPlates([])
-          }
-        </div>
+        <div className="stack">{renderPlates(props.eatenSushis)}</div>
       </div>
     </Fragment>
-  )
-}
+  );
+};
 
-export default Table
+export default Table;
