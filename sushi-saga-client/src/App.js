30895042f1e29596b236b22ee66ff0587146fe3a
myrtokonst <myrtokonst@gmail.com>
2019-05-20 17:39:29 +0100

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -7,11 +7,40 @@ const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
+state = {
+  sushi: [],
+  renderedSushi: [],
+  eatenSushi: []
+}
+
+componentDidMount() {
+  fetch(API).then(resp => resp.json())
+  .then(sushi => this.setState({sushi}))
+  .then(() => this.getFourSushis())
+}
+
+getFourSushis = () => {
+  this.setState({
+    renderedSushi: this.state.sushi.slice(0,4),
+    sushi: this.state.sushi.slice(4)
+  })
+}
+handleClick = (sushi) => {
+  // debugger
+  
+   this.setState({eatenSushi: [...this.state.eatenSushi, sushi]})
+}
+  
   render() {
     return (
       <div className="app">
-        <SushiContainer  />
-        <Table />
+        <SushiContainer sushi={this.state.sushi} 
+                        handleClick={this.handleClick} 
+                        eatenSushi={this.state.eatenSushi} 
+                        renderSushi={this.state.renderedSushi}
+                        getFourSushis={this.getFourSushis}
+                        />
+        <Table sushi={this.state.sushi} eatenSushi={this.state.eatenSushi}/>
       </div>
     );
   }
