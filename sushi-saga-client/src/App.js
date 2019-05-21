52dabc242c496235c0df2423f966ef116d9f35ce
azamkhanuk <azamkhandev@gmail.com>
2019-05-21 10:52:46 +0100

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -6,15 +6,51 @@ import Table from './containers/Table';
 const API = "http://localhost:3000/sushis"
 
 class App extends Component {
+// set all the states here
+  state = {
+    allSushi: [],
+    renderedSushi: [],
+    eatenSushi: [],
+    budget: 100
+  }
 
+  componentDidMount() {
+    this.getSushi()
+      .then(sushi => this.setState({ allSushi: sushi }))
+      .then(() => this.get4Sushi())
+  }
+  // Grab sushi and mount it above + add it to state and then grab the 4 sushis, using () => function
+  getSushi = () => {
+    return fetch(API).then(resp => resp.json())
+  }
+  // Use the getSushi > allSushi to slice to only 4 and put it inside rendered sushi
+  get4Sushi = () => {
+    this.setState({
+      renderedSushi: this.state.allSushi.slice(0, 4),
+      allSushi: this.state.allSushi.slice(4)
+    })
+  }
+  // puts sushi in eatensushi state then takes away budget from the sushi price 
+  storeEatenSushi = sushi => {
+    this.setState({
+      eatenSushi: [...this.state.eatenSushi, sushi],
+      budget: this.state.budget - sushi.price
+    })
+  }
+  // Pass all the state across sushi container and also to table
   render() {
     return (
       <div className="app">
-        <SushiContainer  />
-        <Table />
+        <SushiContainer
+          get4Sushi={this.get4Sushi}
+          fourSushi={this.state.renderedSushi}
+          storeEatenSushi={this.storeEatenSushi}
+          budget={this.state.budget}
+        />
+        <Table eatenSushi={this.state.eatenSushi} budget={this.state.budget} />
       </div>
     );
   }
 }
 
-export default App;
+export default App
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,16 +1,29 @@
-import React, { Fragment } from 'react'
-import MoreButton from '../components/MoreButton'
+import React, { Fragment } from "react"
+import MoreButton from "../components/MoreButton"
+import Sushi from "../components/Sushi"
 
-const SushiContainer = (props) => {
+//! import sushi and everything from APP!
+// functional component using props sushi from APP and all its available methods that were passed down
+const SushiContainer = props => {
+  // maps out the 4 sushis and also passes state from App to sushi component. Key is also passed using the id of sushis. 
+  let renderFourSushi = () => {
+    return props.fourSushi.map(sushi => (
+      <Sushi
+        sushi={sushi}
+        key={sushi.id}
+        storeEatenSushi={props.storeEatenSushi}
+        budget={props.budget}
+      />
+    ))
+  }
+
+  // Above method in render and also passes the get4sushis from App to more button
+
   return (
     <Fragment>
       <div className="belt">
-        {
-          /* 
-             Render Sushi components here!
-          */
-        }
-        <MoreButton />
+        {renderFourSushi()}
+        <MoreButton get4Sushi={props.get4Sushi} />
       </div>
     </Fragment>
   )
